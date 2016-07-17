9767a26b0636a06160daba00dff0dfd8fa21ff26
TRomesh <tharakaromesh@gmail.com>
2016-07-17 13:38:44 +0530

    added a cricle to map

--- mapscript.js
+++ mapscript.js
@@ -1,7 +1,15 @@
 var map;
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 6.9271, lng: 79.8612},
     zoom: 15
   });
+
+  var marker = new google.maps.Marker({
+         position: myLatLng,
+         map: map,
+         title: 'Hello World!'
+       });
+
 }
